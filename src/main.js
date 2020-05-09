6697532953ce7755cffa572b39e6f7d16d57765d
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-09 14:46:38 -0400

    Adición de data para los componentes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,33 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 // Componentes
 var componente2 = Vue.extend({
-  template: '<h3>Listado de Frameworks Javacripts</h3>'
+  template: '<h3>{{subtitulo}}</h3>',
+  data () {
+    return {
+      subtitulo: 'Listado de Frameworks Javacripts'
+    }
+  }
 })
 var componente3 = Vue.extend({
-  template: '<ul><li>Vue.js</li><li>Angular</li><li>React</li></ul>'
+  template: '<ul><li v-for="ele in lista">{{ele.item}}</li></ul>',
+  // Se debe crear un método que devuelva los datos
+  data () {
+    return {
+      lista: [
+        { item: 'Vue.js' },
+        { item: 'Angular' },
+        { item: 'React' },
+        { item: 'Backbone' }
+      ]
+    }
+  }
 })
 Vue.component('componente-saludo', {
+  data () {
+    return {
+      titulo: 'Hola - Introducción a componentes'
+    }
+  },
   methods: {
     display () {
       console.log('componente en linea')
