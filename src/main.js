432b71a66e6925240b6013535da9cac2fd6259a9
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-09 14:33:23 -0400

    Adicionando componentes en línea, ejemplo con un método

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 // Componentes
-var componente1 = Vue.extend({
-  template: '<h1>Hola - Introducción a componentes</h1>'
-})
 var componente2 = Vue.extend({
   template: '<h3>Listado de Frameworks Javacripts</h3>'
 })
 var componente3 = Vue.extend({
   template: '<ul><li>Vue.js</li><li>Angular</li><li>React</li></ul>'
 })
-Vue.component('componente-saludo', componente1)
+Vue.component('componente-saludo', {
+  methods: {
+    display () {
+      console.log('componente en linea')
+    }
+  }
+})
 Vue.component('componente-titulo', componente2)
 Vue.component('componente-lista', componente3)
 
