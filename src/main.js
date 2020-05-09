019cf809e5bd07fed3cf5d4ac51430320555e7d1
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-09 14:25:34 -0400

    Usando extensiones para llamar componentes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 
 Vue.config.productionTip = false
-Vue.component('Saludo', {
+// Componentes
+var componente1 = Vue.extend({
   template: '<h1>Hola - Introducción a componentes</h1>'
-});
-Vue.component('titulo', {
+})
+var componente2 = Vue.extend({
   template: '<h3>Listado de Frameworks Javacripts</h3>'
-});
-Vue.component('lista-frameworks', {
+})
+var componente3 = Vue.extend({
   template: '<ul><li>Vue.js</li><li>Angular</li><li>React</li></ul>'
-});
+})
+Vue.component('componente-saludo', componente1)
+Vue.component('componente-titulo', componente2)
+Vue.component('componente-lista', componente3)
 
 /* eslint-disable no-new */
 new Vue({
